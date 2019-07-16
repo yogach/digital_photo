d4983651d076539efee49bd69b8d5d2b060dd792
yogach2 <471534369@qq.com>
2019-07-16 22:19:46 +0800

    111

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <fonts_manager.h>
 #include <disp_manager.h>
 #include <input_manager.h>
+#include <pic_manager.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -22,7 +23,9 @@ int main (int argc,char * *argv)
 {
 	PT_DispOpr ptDispOpr;
 	int iFdBmp;
-	unsigned char *pucBMPmem;
+	int iRet;
+	unsigned char * pucBMPmem;
+	PT_PicFileParser ptBMPFileParser;
 
 	struct tBMPstat;
 
@@ -36,9 +39,9 @@ int main (int argc,char * *argv)
 	DebugInit ();
 	InitDebugChanel ();
 
-	
+
 	DisplayInit ();
-	
+
 
 	ptDispOpr = GetDispOpr ("fb");
 
@@ -56,24 +59,38 @@ int main (int argc,char * *argv)
 
 	//获取文件大小 将bmp数据mmap到内存上
 	fstat (iFdBmp,&tBMPstat);
-    pucBMPmem = (unsigned char *)mmap(NULL , tBMPstat.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, iFdBmp, 0);
-	if(pucBMPmem == NULL)
-    {
+	pucBMPmem = (unsigned char *)
+	mmap (NULL,tBMPstat.st_size,PROT_READ | PROT_WRITE,MAP_SHARED,iFdBmp,0);
+
+	if (pucBMPmem == NULL)
+	{
 		DBG_PRINTF ("can't mmap bmp file \r\n");
 		return - 1;
 	}
 
-    //初始化图片处理
-	PicFileParserInit();
-		
-    //判断打开的文件是否是bmp文件
-    
+	//初始化图片处理
+	PicFileParserInit ();
 
-    // 提取BMP文件的RGB数据, 缩放, 在LCD上显示出来 
-    
+	//获取BMP测试
+	ptBMPFileParser = GetPicFileParser ("bmp");
 
+	if (ptBMPFileParser == NULL)
+	{
+		DBG_PRINTF ("can't find bmp File Parser \r\n");
+		return - 1;
+	}
+
+	//判断打开的文件是否是bmp文件
+	iRet = ptBMPFileParser->isSupport (pucBMPmem);
+
+	if (iRet == NULL)
+	{
+	    DBG_PRINTF ("this file is not bmp <%s> \r\n",argv[1]);
+		return - 1;
+	}
+
+	// 提取BMP文件的RGB数据, 缩放, 在LCD上显示出来 
 	//设置bmp文件缩放
-	
 	g_tBMPFileParser
 }
 
