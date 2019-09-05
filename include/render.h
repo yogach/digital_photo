614087b36b9dca3ac11d81718459ae31cdfcd2c8
yogach2 <471534369@qq.com>
2019-09-05 23:03:34 +0800

    11

--- include/render.h
+++ include/render.h
@@ -9,4 +9,7 @@ int MergerStringToCenterOfRectangleInVideoMem(int iTopLeftX, int iTopLeftY, int 
 int SetColorForPixelInVideoMem(int iX,int iY,PT_VideoMem ptVideoMem, unsigned int dwBackColor);
 int SetColorForAppointArea(int iTopLeftX, int iTopLeftY, int iBotRightX, int iBotRightY,  PT_VideoMem ptVideoMem, unsigned int dwBackColor);
 
+int isFontInArea ( int iTopLeftX, int iTopLeftY,int iBotRightX, int iBotRightY,PT_FontBitMap ptFontBitMap );
+int MergeOneFontToVideoMem ( PT_FontBitMap ptFontBitMap, PT_VideoMem ptVideoMem );
+
 #endif
